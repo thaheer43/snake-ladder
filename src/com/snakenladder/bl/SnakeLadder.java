1a9e7e36803f52eb6c755f18f76255a02cc59933
thaheer43 <thaheer43@gmail.com>
2022-06-11 13:00:53 +0530

    Added: Logic for check play option

--- src/com/snakenladder/bl/SnakeLadder.java
+++ src/com/snakenladder/bl/SnakeLadder.java
@@ -5,12 +5,29 @@ import java.util.Random;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
-		
-		int player1StartPosition=0;
-		System.out.println(" Player 1's initial position is: " +player1StartPosition);
-		Random random=new Random();
-		int Dice=1+random.nextInt(6);
-		System.out.println(Dice);
+
+		int player1Position = 0;
+		Random random = new Random();
+		int Dice = 1 + random.nextInt(6);
+		System.out.println("dice value: " + Dice);
+		Random random1 = new Random();
+		int playOption = random1.nextInt(3);
+		System.out.println("play option: " + playOption);
+		switch (playOption) {
+		case 0:
+			System.out.println(" NO PLAY " + " Player position is " + player1Position);
+			break;
+		case 1:
+			player1Position = player1Position + Dice;
+			System.out.println(" Ladder!! \n Player position is " + player1Position);
+			break;
+
+		default:
+			player1Position = player1Position - Dice;
+			System.out.println(" Snake " + " Player position is " + player1Position);
+			break;
+		}
+
 	}
 
 }
